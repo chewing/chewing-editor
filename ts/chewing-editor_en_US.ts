f858e5478140d2db0adcb53aa3f35ec55952e053
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2014-03-04 00:14:53 +0800

    Add en_US translation

---
+++ ts/chewing-editor_en_US.ts
@@ -0,0 +1,66 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>AddNewPhraseDialog</name>
+    <message>
+        <location filename="../src/ui/AddNewPhraseDialog.ui" line="26"/>
+        <source>Add new user phrase</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/AddNewPhraseDialog.ui" line="44"/>
+        <source>phrase</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/AddNewPhraseDialog.ui" line="54"/>
+        <source>bopomofo</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>ChewingEditor</name>
+    <message>
+        <location filename="../src/ui/ChewingEditor.ui" line="20"/>
+        <source>Chewing user phrase editor</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/ChewingEditor.ui" line="27"/>
+        <source>import</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/ChewingEditor.ui" line="34"/>
+        <source>export</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/ChewingEditor.ui" line="41"/>
+        <source>add</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/ChewingEditor.ui" line="48"/>
+        <source>remove</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/ui/ChewingEditor.ui" line="55"/>
+        <source>refresh</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Notification</name>
+    <message numerus="yes">
+        <location filename="../src/view/Notification.cpp" line="33"/>
+        <source>Refresh completed. Total %1 user phrase(s).</source>
+        <translation>
+            <numerusform>Refresh completed. Total %1 user phrase.</numerusform>
+            <numerusform>Refresh completed. Total %1 user phrases.</numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
