dd894cb91774664c52a6530c847614376f7766d7
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2014-03-03 21:40:05 +0800

    Add zh_TW translation

---
+++ ts/chewing-editor_zh_TW.ts
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh_TW">
+<context>
+    <name>AddNewPhraseDialog</name>
+    <message>
+        <location filename="../src/ui/AddNewPhraseDialog.ui" line="26"/>
+        <source>AddNewPhraseDialog</source>
+        <translation>新增詞彙</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/AddNewPhraseDialog.ui" line="44"/>
+        <source>phrase</source>
+        <translation>詞彙</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/AddNewPhraseDialog.ui" line="54"/>
+        <source>bopomofo</source>
+        <translation>注音</translation>
+    </message>
+</context>
+<context>
+    <name>ChewingEditor</name>
+    <message>
+        <location filename="../src/ui/ChewingEditor.ui" line="20"/>
+        <source>ChewingEditor</source>
+        <translation>新酷音詞庫編輯器</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/ChewingEditor.ui" line="27"/>
+        <source>import</source>
+        <translation>匯入</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/ChewingEditor.ui" line="34"/>
+        <source>export</source>
+        <translation>匯出</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/ChewingEditor.ui" line="41"/>
+        <source>add</source>
+        <translation>新增</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/ChewingEditor.ui" line="48"/>
+        <source>remove</source>
+        <translation>刪除</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/ChewingEditor.ui" line="55"/>
+        <source>refresh</source>
+        <translation>更新</translation>
+    </message>
+</context>
+</TS>
