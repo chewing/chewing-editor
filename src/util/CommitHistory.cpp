7d83db3f690c79db184d314ae3eebf2047adfa11
Stanley Ding <stanleyding.tw@gmail.com>
2014-08-22 20:27:21 +0800

    Add class CommitHistory

---
+++ src/util/CommitHistory.cpp
@@ -0,0 +1,32 @@
+/*
+ * chewing-editor: Chewing userphrase editor
+ * Copyright (C) 2014 Chewing Development Team
+
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "CommitHistory.h"
+
+CommitHistory::CommitHistory(const int& length, const char *phrase, const unsigned short *phones)
+    :length_{length}
+    ,phrase_{phrase}
+    ,phones_{}
+    ,display_{}
+{
+    while (*phones != 0) {
+        phones_.push_back(*phones++);
+    }
+    display_ = QString("%1 %2").arg(length).arg(phrase);
+}
