f2a6dd24f24bd22b26b260ca7700110cfc029d81
Chocobo1 <Chocobo1@users.noreply.github.com>
2016-03-25 14:03:31 +0800

    Grab focus to phrase input widget on dialog open. Closes #128.

--- src/view/UserphraseDialog.cpp
+++ src/view/UserphraseDialog.cpp
@@ -59,4 +59,6 @@ void UserphraseDialog::setText(const QString& phrase, const QString& bopomofo)
 {
     ui_.get()->phrase->setText(phrase);
     ui_.get()->bopomofo->setText(bopomofo);
+
+    ui_->phrase->setFocus(Qt::PopupFocusReason);
 }
